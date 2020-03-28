454c746a30e83cfae5a147bda37480ccb45e5e6d
Andrew Farley <16amf8@queensu.ca>
2020-03-28 09:57:53 -0400

    Refactoring, added actual host function

--- MP3_Andrew_Farley/MP3_Andrew_Farley/kernel.cu
+++ MP3_Andrew_Farley/MP3_Andrew_Farley/kernel.cu
@@ -17,12 +17,10 @@ __global__ void MatMult(float *a, float *b, float *c) {
 	int j = blockIdx.y * blockDim.y + threadIdx.y;
 
 	int index = i + j * N;
-	float total = 0;
 	for (int k = 0; k < N; k++) {
 		int a_index = i + k * N;
 		int b_index = k + j * N;
 		if (i < N && j < N) {
-			total += a[a_index] * b[b_index];
 			c[index] += a[a_index] * b[b_index];
 		}
 	}
@@ -31,62 +29,58 @@ __global__ void MatMult(float *a, float *b, float *c) {
 
 typedef float myMat[N*N];
 
+void HostFunction(myMat* A, myMat* B, myMat* C);
+
+size_t dsize;
+
 int main() {
 	myMat *A, *B, *C;
-	size_t dsize = N*N*sizeof(float);
+	dsize = N*N*sizeof(float);
 	A = (myMat*)malloc(dsize);
 	B = (myMat*)malloc(dsize);
 	C = (myMat*)malloc(dsize);
 
+	HostFunction(A, B, C);
+
+	getc(stdin);
+
+	return 0;
+}
+
+void HostFunction(myMat* A, myMat* B, myMat* C) {
+	//Initialize matricies
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			int index = i + j * N;
-			(*A)[index] = 2.0 + i;//10 * (float)rand() / (float)RAND_MAX;
-			(*B)[index] = 4.0 + j;//10 * (float)rand() / (float)RAND_MAX;
+			(*A)[index] = 10 * (float)rand() / (float)RAND_MAX;
+			(*B)[index] = 10 * (float)rand() / (float)RAND_MAX;
 			(*C)[index] = 0.0f;
 		}
 	}
 
+	//Pointers to matricies
 	float *pA, *pB, *pC;
 
-	// allocate matrices in device memory
+	//Allocate matrices in device memory
 	cudaMalloc((void**)&pA, (N*N)*sizeof(float));
 	cudaMalloc((void**)&pB, (N*N)*sizeof(float));
 	cudaMalloc((void**)&pC, (N*N)*sizeof(float));
 
-	printf("cudaMemcpy\n");
-	// copy matrices from host memory to device memory
+	//Copy matrices from host memory to device memory
 	cudaMemcpy(pA, A, (N*N)*sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpy(pB, B, (N*N)*sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpy(pC, C, (N*N)*sizeof(float), cudaMemcpyHostToDevice);
 
-	// KERNEL INVOCATION
-	// each thread produces 1 output matrix element
+	//KERNEL CALL
+	//Each thread produces 1 output matrix element
 	dim3 threadsPerBlock(BLOCKDIM, BLOCKDIM);
 	dim3 numBlocks((int)ceil(N / (float)threadsPerBlock.x), (int)ceil(N / (float)threadsPerBlock.y));
-	MatMult<<<numBlocks, threadsPerBlock>>>(pA, pB, pC);
+	MatMult << <numBlocks, threadsPerBlock >> >(pA, pB, pC);
 
-	// copy result from device memory to host memory
+	//Copy result from device memory to host memory
 	cudaMemcpy(C, pC, (N*N)*sizeof(float), cudaMemcpyDeviceToHost);
 
-	/*printf("Array A = \n");
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			int index = i + j * N;
-			printf("%f ", (*A)[index]);
-		}
-		printf("\n");
-	}
-
-	printf("Array B = \n");
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			int index = i + j * N;
-			printf("%f ", (*B)[index]);
-		}
-		printf("\n");
-	} */
-
+	//Compute matrix multiplication using the CPU
 	myMat *CTemp;
 	CTemp = (myMat*)malloc(dsize);
 	for (int i = 0; i < N; i++) {
@@ -101,6 +95,7 @@ int main() {
 		}
 	}
 
+	//Compare GPU computed multiplication to CPU
 	int good = 1;
 	int i, j;
 	//printf("Array C = \n");
@@ -110,26 +105,24 @@ int main() {
 			float val = (*C)[index];
 			//printf("%f ", val);
 			float diff = (*CTemp)[index] - val;
-			if (absf(diff) > TOLERANCE) {
+			/*if (absf(diff) > TOLERANCE) {
 				printf("%d %d %f %f %f\n", i, j, val, (*CTemp)[index], diff);
 				good = 0;
-			}
+			}*/
 		}
 		//printf("\n");
 	}
 
 	if (good == 1) {
 		printf("TEST PASSED\n");
+	} else {
+		printf("TEST FAILED\n");
 	}
 
 	// free device memory
 	cudaFree(pA);
 	cudaFree(pB);
 	cudaFree(pC);
-
-	getc(stdin);
-
-	return 0;
 }
 
 float absf(float n) {
